13057d8ab30b93466188ff95b891f382d86fa730
Anthony <129002769+Flikcer@users.noreply.github.com>
2023-05-16 13:47:43 -0400

    basic routes

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
+//path to index upon url req to '/' and '/index.html' () and ? make the html optional
+app.get("^/$|/index(.html)?", (req, res) => {
   try {
     //same thing as commented code but this allows for any type of pathing so it doesnt break wehn deploying to certain places or building
     res.status(200).sendFile(path.join(__dirname, "views", "index.html"));
@@ -14,12 +15,26 @@ app.get("/", (req, res) => {
   }
 });
 
-app.get("/new-page.html", (req, res) => {
+app.get("/new-page(.html)?", (req, res) => {
   try {
     res.status(200).sendFile(path.join(__dirname, "views", "new-page.html"));
   } catch (err) {
     console.error(err);
   }
+});
+
+app.get("/old-page(.html)?", (req, res) => {
+  try {
+    //dont have to use status here, will take it in as first param
+    res.redirect(301, "/new-page.html");
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.get("/*", (req, res) => {
+  //send to 404 with proper code
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
